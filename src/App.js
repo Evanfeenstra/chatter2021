4e3130409caf14f9465c9e525e8c23e54049f253
Evanfeenstra <evanfeenstra@gmail.com>
2021-02-10 10:26:02 -0800

    add routes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import TextInput from './TextInput';
 import Message from './Message'
 import NamePicker from './NamePicker'
 import {db, useDB} from './db'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  const messages = useDB()
+export default function Wrap() {
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/:room" component={App} />
+    </Switch>
+  </BrowserRouter>
+}
+
+function App(props) {
+  const room = props.match.params.room || 'home'
+
+  const messages = useDB(room)
   const [username,setUsername] = useState(
     localStorage.getItem('username') || ''
   )
 
-  console.log(messages)
+  // console.log(messages)
   return <div className="App">
  
     <header className="header">
@@ -29,10 +41,8 @@ function App() {
     </main>
 
     <TextInput 
-      send={(t)=> db.send({text:t, name:username, date:new Date()})}
+      send={(t)=> db.send({text:t, name:username, date:new Date(), room})}
     />
 
   </div>
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ASDF from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ASDF />
   </React.StrictMode>,
   document.getElementById('root')
 );
